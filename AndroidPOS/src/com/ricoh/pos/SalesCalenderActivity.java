ac183ad7e0dc1c6344ced7ee6a240d4ece7162db
arai-wa <wataru.arai@jrits.ricoh.co.jp>
2015-12-03 16:17:26 +0900

    BOP-25 タブレットで週次・月次それぞれの売り上げと利益を見る
    * カレンダーにそれぞれの日の売上と利益を表示するように修正した
    * カレンダーのそれぞれの日の背景色を変更した
        * 前日比で利益が増加していれば青色
        * 前日比で利益が減少していれば赤色

--- AndroidPOS/src/com/ricoh/pos/SalesCalenderActivity.java
+++ AndroidPOS/src/com/ricoh/pos/SalesCalenderActivity.java
@@ -1,68 +1,110 @@
 package com.ricoh.pos;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
+import android.widget.TextView;
 
 import com.caldroid.CaldroidFragment;
+import com.caldroid.CaldroidGridAdapter;
 import com.caldroid.CaldroidListener;
 import com.ricoh.pos.data.SingleSalesRecord;
 import com.ricoh.pos.model.SalesCalenderManager;
 import com.ricoh.pos.model.SalesRecordManager;
 
+import org.joda.time.DateTime;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+
 public class SalesCalenderActivity extends FragmentActivity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.activity_sales_calender);
 
-		CaldroidFragment caldroidFragment = new CaldroidFragment();
+		CaldroidFragment caldroidFragment = new SalesTrendFragment();
 		Bundle args = new Bundle();
 		Calendar cal = Calendar.getInstance();
 		args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
 		args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
 		caldroidFragment.setArguments(args);
-		
+
 		addCalenderListener(caldroidFragment);
 
 		FragmentTransaction t = getSupportFragmentManager().beginTransaction();
 		t.replace(R.id.calendar1, caldroidFragment);
 		t.commit();
-		
+
 	}
-	
-	private void addCalenderListener(CaldroidFragment fragment){
-		CaldroidListener listener = new CaldroidListener(){
+
+	private void addCalenderListener(CaldroidFragment fragment) {
+		CaldroidListener listener = new CaldroidListener() {
 
 			@Override
 			public void onSelectDate(Date date, View view) {
-				
-				ArrayList<SingleSalesRecord> salesRecordsOfTheDay = 
+
+				ArrayList<SingleSalesRecord> salesRecordsOfTheDay =
 						SalesRecordManager.getInstance().restoreSingleSalesRecordsOfTheDay(date);
-				
+
 				if (salesRecordsOfTheDay.size() == 0) {
 					// No sales record.
 					// TODO: show alert dialog or something
 					return;
-				} 
-				
+				}
+
 				SalesCalenderManager.getInstance().setSelectedDate(date);
-				
+
 				Intent intent = new Intent(SalesCalenderActivity.this, SalesRecordListActivity.class);
 				startActivity(intent);
 			}
-			
+
 		};
-		
+
 		fragment.setCaldroidListener(listener);
 	}
 
+
+	public static class SalesTrendFragment extends CaldroidFragment {
+
+		@Override
+		public CaldroidGridAdapter getNewDatesGridAdapter(int month, int year) {
+			return new SalesTrendGridAdapter(getActivity(), month, year, getCaldroidData(), extraData);
+		}
+
+		private static class SalesTrendGridAdapter extends CaldroidGridAdapter {
+			public SalesTrendGridAdapter(Context context, int month, int year, HashMap<String, Object> caldroidData, HashMap<String, Object> extraData) {
+				super(context, month, year, caldroidData, extraData);
+			}
+
+			@Override
+			protected void setCustomResources(DateTime dateTime, TextView textView) {
+				super.setCustomResources(dateTime, textView);
+
+
+				Date today = dateTime.toDate();
+
+				double todaySales = SalesRecordManager.getInstance().getOneDayTotalSales(today);
+				double todayProfit = SalesRecordManager.getInstance().getOneDayTotalNetProfit(today);
+
+				textView.setText(textView.getText() + "\n\n" + todaySales + "\n" + todayProfit);
+
+
+				double yesterdayProfit = SalesRecordManager.getInstance().getOneDayTotalNetProfit(dateTime.minusDays(1).toDate());
+
+				if (yesterdayProfit < todayProfit) {
+					textView.setBackgroundResource(R.color.profit_increased);
+				} else if (todayProfit < yesterdayProfit) {
+					textView.setBackgroundResource(R.color.profit_degreased);
+				}
+			}
+		}
+	}
 }
